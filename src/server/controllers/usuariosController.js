9584db0fc14bef9ae33a18eb3707516bab1e371c
carol <anacsl@dcc.ufrj.br>
2025-11-27 17:31:54 -0300

    Atualizar usuariosController.js

--- src/server/controllers/usuariosController.js
+++ src/server/controllers/usuariosController.js
@@ -21,7 +21,11 @@ exports.criarUsuario = async (req, res) => {
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
             return res.status(400).json({ message: 'Nome de usuário e senha são obrigatórios.' });
+=======
+            return res.status(400).json({ message: 'Nome de usuário e senha são obrigatórios. Tente novamente!' });
+>>>>>>> Stashed changes
 =======
             return res.status(400).json({ message: 'Nome de usuário e senha são obrigatórios. Tente novamente!' });
 >>>>>>> Stashed changes
@@ -39,8 +43,11 @@ exports.criarUsuario = async (req, res) => {
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
             return res.status(400).json({ message: 'Utilize no mínimo 3 caracteres para o nome e 5 caracteres para a senha.' });
 =======
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 =======
