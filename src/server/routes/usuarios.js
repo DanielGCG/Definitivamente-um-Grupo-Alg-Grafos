6c5cfccad9fbc65db9f1cb3f04343b91a89758dc
DanielGCG <danielgcg@dcc.ufrj.br>
2025-11-27 18:13:38 -0300

    Adicionado busca de usuários usando o BD

--- src/server/routes/usuarios.js
+++ src/server/routes/usuarios.js
@@ -6,5 +6,6 @@ router.get('/', controller.listarUsuariosRanking);
 router.post('/register', controller.criarUsuario);
 router.put('/', controller.atualizarUsuario);
 router.post('/login', controller.loginUsuario);
+router.get('/search', controller.listarUsuariosPorNome);
 
 module.exports = router;
